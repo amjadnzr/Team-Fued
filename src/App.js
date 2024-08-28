55f6540945a9199de413614deeb95b80dc516989
Amjad Nazar <amjad@corzent.com>
2024-08-28 17:29:29 +0530

    Added minor changes

--- src/App.js
+++ src/App.js
@@ -11,27 +11,51 @@ function App() {
   const [currentTeamIndex, setCurrentTeamIndex] = useState(0);
   const [pointsToAdd, setPointsToAdd] = useState(0);
   const [pointsToDeduct, setPointsToDeduct] = useState(0);
-  const [wrongAnswer, setWrongAnswer] = useState(false); // State to track wrong answer
+  const [wrongAnswer, setWrongAnswer] = useState(false);
+  const [suggestions, setSuggestions] = useState([]);
+
+  // List of members for suggestions
+  const members = jsonData.members
 
   useEffect(() => {
-    // Load data from local storage if it exists
+
+    let currentQuestion = data.questions[0]
+    if (currentQuestion) {
+      let teamIdx = jsonData.teams.findIndex(team => team.name === currentQuestion.defaultTeam)
+      setCurrentTeamIndex(teamIdx)
+    }
+
     const storedData = localStorage.getItem('gameData');
-  
     if (storedData) {
       setData(JSON.parse(storedData));
-      setTrigger(true)
+      setTrigger(true);
     }
   }, []);
 
   useEffect(() => {
-    // Save data to local storage whenever it changes
     if (trigger) {
-    localStorage.setItem('gameData', JSON.stringify(data));
+      localStorage.setItem('gameData', JSON.stringify(data));
     }
   }, [data]);
 
   const handleInputChange = (e) => {
-    setInput(e.target.value);
+    const value = e.target.value;
+    setInput(value);
+
+    // Filter suggestions based on input
+    if (value.length > 0) {
+      const filteredSuggestions = members.filter((member) =>
+        member.toLowerCase().startsWith(value.toLowerCase())
+      );
+      setSuggestions(filteredSuggestions);
+    } else {
+      setSuggestions([]);
+    }
+  };
+
+  const handleSuggestionClick = (suggestion) => {
+    setInput(suggestion);
+    setSuggestions([]);
   };
 
   const handlePointsToAddChange = (e) => {
@@ -51,7 +75,6 @@ function App() {
       if (!revealedAnswers.includes(answer)) {
         setRevealedAnswers([...revealedAnswers, answer]);
 
-        // Update points for the current team
         const updatedTeams = data.teams.map((team, index) => {
           if (index === currentTeamIndex) {
             return { ...team, points: team.points + answer.points };
@@ -61,19 +84,15 @@ function App() {
         setData({ ...data, teams: updatedTeams });
 
         setInput("");
-        setWrongAnswer(false); // Reset wrong answer flag
+        setWrongAnswer(false);
 
-        // Update local storage after submitting an answer
         localStorage.setItem('gameData', JSON.stringify(data));
       }
     } else {
-      // Deduct points for the current team and change to next team
       handleTeamPointsDeduct(currentTeamIndex, pointsToDeduct);
-      setWrongAnswer(true); // Set wrong answer flag
+      setWrongAnswer(true);
       setCurrentTeamIndex((prevIndex) => (prevIndex + 1) % data.teams.length);
       setInput("");
-
-      // Update local storage after a wrong answer
       localStorage.setItem('gameData', JSON.stringify(data));
     }
   };
@@ -87,36 +106,43 @@ function App() {
     });
     setData({ ...data, teams: updatedTeams });
 
-    // Update local storage after adding points
     localStorage.setItem('gameData', JSON.stringify(data));
   };
 
   const handleTeamPointsDeduct = (teamIndex, points) => {
     const updatedTeams = data.teams.map((team, index) => {
       if (index === teamIndex) {
-        return { ...team, points: Math.max(team.points - points, 0) }; // Prevent negative points
+        return { ...team, points: Math.max(team.points - points, 0) };
       }
       return team;
     });
     setData({ ...data, teams: updatedTeams });
 
-    // Update local storage after deducting points
     localStorage.setItem('gameData', JSON.stringify(data));
   };
 
   const nextQuestion = () => {
     setRevealedAnswers([]);
+    const nextquestion = data.questions[(currentQuestionIndex + 1) % data.questions.length]
+    if (nextquestion){
+      const defaultTeam = jsonData.teams.findIndex(team => team.name === nextquestion.defaultTeam)
+      setCurrentTeamIndex(defaultTeam)
+    }
     setCurrentQuestionIndex((prevIndex) => (prevIndex + 1) % data.questions.length);
-
-    // Update local storage after changing the question
+ 
     localStorage.setItem('gameData', JSON.stringify(data));
   };
 
   const prevQuestion = () => {
     setRevealedAnswers([]);
-    setCurrentQuestionIndex((prevIndex) => (prevIndex - 1 + data.questions.length) % data.questions.length);
 
-    // Update local storage after changing the question
+    const prevQuestion = data.questions[(currentQuestionIndex - 1) % data.questions.length]
+    if (prevQuestion){
+      const defaultTeam = jsonData.teams.findIndex(team => team.name === prevQuestion.defaultTeam)
+      setCurrentTeamIndex(defaultTeam)
+    }
+
+    setCurrentQuestionIndex((prevIndex) => (prevIndex - 1 + data.questions.length) % data.questions.length);
     localStorage.setItem('gameData', JSON.stringify(data));
   };
 
@@ -126,23 +152,19 @@ function App() {
 
   const revealAllAnswers = () => {
     setRevealedAnswers(data.questions[currentQuestionIndex].answers);
-
-    // Update local storage after revealing all answers
     localStorage.setItem('gameData', JSON.stringify(data));
   };
 
   return (
-  
     <div className="p-4 flex">
-      {data != {} ? (
+      {data ? (
         <>
-          {/* Question Section */}
           <div className="w-2/3 pr-4">
             <h1 className="text-2xl font-bold mb-4">Team Feud</h1>
   
             <div className="mb-4">
               <h2 className="question-text">{data.questions[currentQuestionIndex].question}</h2>
-              <div className="flex items-center">
+              <div className="flex items-center relative">
                 <input
                   type="text"
                   className="border p-2 mt-4 flex-1"
@@ -150,6 +172,19 @@ function App() {
                   onChange={handleInputChange}
                   placeholder="Type an answer..."
                 />
+                {suggestions.length > 0 && (
+                  <ul className="absolute bg-white border mt-12 w-full">
+                    {suggestions.map((suggestion, index) => (
+                      <li
+                        key={index}
+                        className="p-2 cursor-pointer hover:bg-gray-200"
+                        onClick={() => handleSuggestionClick(suggestion)}
+                      >
+                        {suggestion}
+                      </li>
+                    ))}
+                  </ul>
+                )}
                 <button
                   onClick={handleAnswerSubmit}
                   className="ml-2 bg-blue-500 text-white p-2 rounded"
@@ -158,13 +193,13 @@ function App() {
                 </button>
               </div>
             </div>
-  
+
             {wrongAnswer && (
               <div className="mb-4 text-red-500">
                 Wrong Answer! The next team gets a chance.
               </div>
             )}
-  
+
             <div className="mb-4">
               {data.questions[currentQuestionIndex].answers.map((answer, index) => (
                 <div
@@ -182,7 +217,7 @@ function App() {
                 </div>
               ))}
             </div>
-  
+
             <div className="mb-4">
               <button
                 onClick={prevQuestion}
@@ -196,7 +231,7 @@ function App() {
               >
                 Next Question
               </button>
-  
+
               <button
                 onClick={revealAllAnswers}
                 className="bg-yellow-500 text-white p-2 rounded"
@@ -205,8 +240,7 @@ function App() {
               </button>
             </div>
           </div>
-  
-          {/* Team Points Section */}
+
           <div className="w-1/3 pl-4">
             <div className="mb-4">
               <div className="flex items-center mb-2">
@@ -228,7 +262,7 @@ function App() {
                 />
               </div>
             </div>
-  
+
             <div className="mb-4">
               {data.teams.map((team, index) => (
                 <div key={index} className={`mb-2 p-2 border rounded ${index === currentTeamIndex ? 'bg-yellow-100' : ''}`}>
@@ -249,7 +283,7 @@ function App() {
                 </div>
               ))}
             </div>
-  
+
             <div className="mb-4">
               <label className="mr-2">Select Current Team:</label>
               <select value={currentTeamIndex} onChange={handleTeamChange} className="border p-2">
@@ -270,6 +304,3 @@ function App() {
 }
 
 export default App;
-
-
-
